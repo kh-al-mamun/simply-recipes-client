dd3af6523ffd3eaea35a87eb2f4b0ec451dd433f
khmamun <khmamun27@gmail.com>
2023-05-04 05:21:59 +0600

    home banner added

---
+++ src/pages/Shared/BannerHone/BannerHome.jsx
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './BannerHome.css';
+
+const BannerHome = () => {
+    return (
+        <div className="banner-home" >
+            <div className="banner-head"> 
+                <div className="row2-text">
+                    <h1>Simply Recipes</h1> 
+                    <h4>no fluff, just recipes</h4>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BannerHome;
