f89bb25edf040022e88225c6dc75f5769aa2f69f
khmamun <khmamun27@gmail.com>
2023-05-04 05:22:51 +0600

    home banner updated

---
+++ src/pages/Shared/BannerHone/BannerBook.jsx
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './BannerBook.css';
+
+const BannerBook = () => {
+    return (
+        <div className='section banner-book-background'>
+            <div className='banner-book-overlay'></div>
+            <div className='banner-book'>
+                <div className='desc'>
+                    <h4 className='special-dish-text'>Special Dish</h4>
+                    <h2>Get the book of most popular recipe</h2>
+                    <p></p>
+                    <button className='btn'>Get Your Copy!</button>
+                </div>
+                <div>
+                    <img src="assets/book.png" alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BannerBook;
