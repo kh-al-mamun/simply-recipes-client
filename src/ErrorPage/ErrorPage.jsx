3a33c2ca06f6e5e63fedb0061eccf40717984fd6
khmamun <khmamun27@gmail.com>
2023-05-04 04:04:17 +0600

    error page added

---
+++ src/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './ErrorPage.css';
+
+const ErrorPage = () => {
+    return (
+        <div className='error-page'>
+            <p>Page Not Found!</p>
+        </div>
+    );
+};
+
+export default ErrorPage;
